2462045008eb3795ffded55e916200db9a95ab6c
Vinayak <153752985+Vinayakmangalagi@users.noreply.github.com>
2025-02-07 18:58:04 +0530

    abstract

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+abstract class Employee{
+    private String name;
+    private int id;
+
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+
+    abstract double calculateSalary();
+
+    @Override
+    public String toString(){
+        return "Employee[name="+name+", id="+id+", salary="+calculateSalary()+"]";
+    }
+}
+
+class FullTimeEmployee extends Employee{
+
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    double calculateSalary() {
+        return 0;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
